9aaee6ade505e7d7027b8b126d192233c906fcd2
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-07 15:14:38 -0400

    Update index.stories.ts

--- dropdown/index.stories.ts
+++ dropdown/index.stories.ts
@@ -1,3 +1,4 @@
-import { Button } from '../fluentui/react-components';
+import { Button, ButtonProps } from "@fluentui/react-components";
+import * as React from "react";
 
 export const story1 = () => "<Button appearance="primary">Get started</Button>";
