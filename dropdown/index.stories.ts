95b3523cc7e1bd56d05ec14d054a6a7b13d30a0b
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-07 15:20:29 -0400

    Update index.stories.ts

--- dropdown/index.stories.ts
+++ dropdown/index.stories.ts
@@ -1,4 +1,6 @@
 import { Button, ButtonProps } from "@fluentui/react-components";
 import * as React from "react";
 
-export const story1 = () => "<Button appearance='primary' icon={<CalendarMonthRegular />}>Primary</Button>";
+export const allowToggle = () => (
+    <Button appearance='primary' icon={<CalendarMonthRegular />}>Primary</Button>
+);
