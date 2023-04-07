0162c5b755bf87098f13324567eb917c7c33377f
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-07 14:34:30 -0400

    xx

--- dropdown/index.stories.ts
+++ dropdown/index.stories.ts
@@ -1,2 +1,23 @@
-export const story1 = () => "Hello World";
-export const story2 = () => "Story 2";
+# Button
+
+This is a button component.
+
+```js script
+import { html } from 'lit';
+import '~/tokens/variables.css';
+import '../src/button.css';
+```
+
+```js preview-story
+export const base = () => html` <button class="btn">Click me!</button> `;
+```
+
+```js preview-story
+export const outline = () =>
+  html` <button class="btn" outline>Click me!</button> `;
+```
+
+```js preview-story
+export const disabled = () =>
+  html` <button class="btn" disabled>Click me!</button> `;
+```
