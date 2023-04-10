19a08a0e6d83c90a3e48d9afcdfa71a5eb3b7921
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-10 10:01:57 -0400

    Revert "Update index.stories.tsx"
    This reverts commit 6b810a808da08da410c02194072e283401c00fd5.

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FluentProvider, teamsLightTheme, makeStyles, Button } from '@fluentui/react-components';
 
+
 const useStyles = makeStyles({
   wrapper: {
     columnGap: "15px",
@@ -10,10 +11,12 @@ const useStyles = makeStyles({
 });
   const styles = useStyles();
 export const FluentButton = () => (
-       <>
+  const styles = useStyles();
+  return (
+    <div className={styles.wrapper}>
         <FluentProvider theme={teamsLightTheme}>
            <Button appearance='primary'>Primary</Button>
            <Button shape="circular">Circular</Button>
         </FluentProvider>
-    </>
+ </div>
 );
