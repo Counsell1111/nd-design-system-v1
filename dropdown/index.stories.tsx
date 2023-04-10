4cb8c64c6946dd19e4c850413a03e915d83f154b
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-10 10:09:29 -0400

    Update index.stories.tsx

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FluentProvider, teamsLightTheme, makeStyles, Button } from '@fluentui/react-components';
 
-ReactDOM.render(
-  <FluentProvider theme={teamsLightTheme}>
 
-  </FluentProvider>,
-  document.getElementById('root'),
-);
+const ExampleComponent {
+return (
+  <>
+  <Button shape="circular" appearance='primary'>Primary</Button>
+  <Button shape="circular">Circular</Button>
+  </>
+  );
+};
 
 export const FluentButton = () => (
        <>
         <FluentProvider theme={teamsLightTheme}>
-           <Button shape="circular" appearance='primary'>Primary</Button>
-           <Button shape="circular">Circular</Button>
+        <ExampleComponent />
         </FluentProvider>
     </>
 );
