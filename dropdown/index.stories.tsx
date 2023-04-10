bf0701a31838310494d1f4220b3fbd93f8ad900c
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-10 10:41:24 -0400

    Update index.stories.tsx

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -4,9 +4,7 @@ import { FluentProvider, teamsLightTheme, makeStyles, Button } from '@fluentui/r
 
 export const FluentButton = () => (
        <>
-        <FluentProvider theme={teamsLightTheme}>
         <Button shape="circular" appearance='primary'>Primary</Button>
         <Button shape="circular">Circular</Button>
-        </FluentProvider>
     </>
 );
