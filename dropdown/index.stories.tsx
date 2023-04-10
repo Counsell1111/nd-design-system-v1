e8390ea5a53665261719788a8c89327e9867980f
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-10 10:11:36 -0400

    Update index.stories.tsx

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -2,8 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FluentProvider, teamsLightTheme, makeStyles, Button } from '@fluentui/react-components';
 
+import {
+  bundleIcon,
+  CalendarMonthFilled,
+  CalendarMonthRegular,
+} from "@fluentui/react-icons";
 
-  export const ExampleComponent = () => {
+const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
+
+const useStyles = makeStyles({
+  wrapper: {
+    columnGap: "15px",
+    display: "flex",
+  },
+});
+
+export const ExampleComponent = () => {
+  const styles = useStyles();
 return (
   <>
   <Button shape="circular" appearance='primary'>Primary</Button>
