7740ca527ca883f1d7c749a93a19bf374ef9c1a8
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-10 10:05:35 -0400

    Update index.stories.tsx

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FluentProvider, teamsLightTheme, makeStyles, Button } from '@fluentui/react-components';
 
+ReactDOM.render(
+  <FluentProvider theme={teamsLightTheme}>
+
+  </FluentProvider>,
+  document.getElementById('root'),
+);
+
 export const FluentButton = () => (
        <>
         <FluentProvider theme={teamsLightTheme}>
