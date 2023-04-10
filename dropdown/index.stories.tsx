b966f5141e30d314f92db017fe64fd8a338b647f
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-10 10:02:20 -0400

    Update index.stories.tsx

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -2,21 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { FluentProvider, teamsLightTheme, makeStyles, Button } from '@fluentui/react-components';
 
-
 const useStyles = makeStyles({
   wrapper: {
     columnGap: "15px",
     display: "flex",
   },
 });
-  const styles = useStyles();
+const styles = useStyles();
 export const FluentButton = () => (
-  const styles = useStyles();
-  return (
-    <div className={styles.wrapper}>
+       <>
         <FluentProvider theme={teamsLightTheme}>
-           <Button appearance='primary'>Primary</Button>
+        <div className={styles.wrapper}>
+           <Button shape="circular" appearance='primary'>Primary</Button>
            <Button shape="circular">Circular</Button>
+           </div>
         </FluentProvider>
- </div>
+    </>
 );
