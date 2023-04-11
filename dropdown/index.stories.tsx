48b9ce490aab9a23e5aa1b71e1a162275270dd5e
Counsell1111 <jacob.counsell@netdocuments.com>
2023-04-11 10:26:03 -0400

    Update index.stories.tsx

--- dropdown/index.stories.tsx
+++ dropdown/index.stories.tsx
@@ -1,3 +1,12 @@
-export const FluentButton = () => (
-        <button>Primary</button>
-);
+import React from 'react';
+import { FluentProvider, teamsLightTheme, Button } from '@fluentui/react-components';
+
+export const Shape = () => {
+    <>
+    <FluentProvider theme={teamsLightTheme}>
+      <Button>Rounded</Button>
+      <Button shape="circular">Circular</Button>
+      <Button shape="square">Square</Button>
+    </FluentProvider>
+    </>
+}
